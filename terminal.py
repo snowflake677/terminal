87a124f0b1dc77374a47b44fb93b399ed20ef51c
snowflake677 <109577632+snowflake677@users.noreply.github.com>
2023-08-29 13:38:14 +0200

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,31 @@
+import os,runpy,sys
+os.system('title terminal')
+os.system('@mode con cols=150 lines=60')
+dir=sys.path[0]
+perms="#"
+os.system(":terminal")
+on=True
+while on:
+    command = input(dir+perms)
+    
+    if command.startswith("exit"):
+        on=False
+    
+    elif command.startswith("cd"):
+        parts = command.split()
+        command = parts[0]
+        args = parts[1:]
+        if args[0] =="..":
+            dir=os.path.split(dir)[0]
+        else:
+            dir=dir+"\\"+args[0]
+    else:
+        if command != "":
+            try:
+                if " " in command:
+                    parts = command.split()
+                    command = parts[0]
+                    args = parts[1:]
+                runpy.run_path("commands\\"+command)
+            except:
+                print("no command found try help")
