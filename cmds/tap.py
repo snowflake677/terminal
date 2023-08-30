c64ca7e3eabaaf17ad1c2952ac865a30dc547262
snowflake677 <109577632+snowflake677@users.noreply.github.com>
2023-08-30 12:48:54 +0200

    Update tap.py

--- cmds/tap.py
+++ cmds/tap.py
@@ -1,7 +1,12 @@
-import sys,requests,terminal
+import sys,requests,os
 sys.path.append("/")
 def get(file):
-    r = requests.get("https://raw.githubusercontent.com/snowflake677/terminal/packages/"+file, allow_redirects=True)
+    os.mkdir(sys.path[0]+"\\opt\\"+file)
+    r = requests.get("https://raw.githubusercontent.com/snowflake677/terminal/packages/"+file+"/"+file+".py", allow_redirects=True)
+    open(sys.path[0]+"\\opt\\"+file+"\\"+file+".py", 'wb').write(r.content)
+    r = requests.get("https://raw.githubusercontent.com/snowflake677/terminal/packages/"+file+"/config.py", allow_redirects=True)
+    open(sys.path[0]+"\\opt\\"+file+"\\config.py", 'wb').write(r.content)
+    r = requests.get("https://raw.githubusercontent.com/snowflake677/terminal/packages/"+file+"/cmd", allow_redirects=True)
     open(sys.path[0]+"\\commands\\"+file, 'wb').write(r.content)
 def tap(arg):
     if arg == "-h":
