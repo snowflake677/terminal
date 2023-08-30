c331457508861d9dfb21f324285619f7f0510f04
snowflake677 <109577632+snowflake677@users.noreply.github.com>
2023-08-30 13:13:24 +0200

    Update homebrew.py

--- homebrew/homebrew.py
+++ homebrew/homebrew.py
@@ -1,11 +1,11 @@
-import sys,requests,opt.homebrew.config as conf,cmds.text as t
+import sys,requests,cmds.homebrewconfig as conf,cmds.text as t
 sys.path.append("/")
 
 def get(file):
     r = requests.get(conf.source+file, allow_redirects=True)
     open(sys.path[0]+"\\commands\\"+file, 'wb').write(r.content)
-  
-if terminal.args[0] == "-h":
-    print("homebrew\nhomebrew [package name]")
-else:
-		t.print_with_color(conf.logo, color=Back.CYAN)
+def hb(arg):
+    if arg == "-h":
+        print("homebrew\nhomebrew [package name]")
+    else:
+	t.print_with_color(conf.logo, color=Back.CYAN)
