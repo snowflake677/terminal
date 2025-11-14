866391ed7d0200591db231d071b5e5081d53c178
cf677 <109577632+snowflake677@users.noreply.github.com>
2025-11-14 17:52:34 +0100

    Update touch.py

--- cmds/touch.py
+++ cmds/touch.py
@@ -2,6 +2,6 @@ import sys
 sys.path.append("/")
 def toutch(arg):
     if arg == "-h":
-        print("creats a file\ntoutch [file name]")
+        print("creats a file\ntouch [file name]")
     else:
         open(arg,"w+").close
